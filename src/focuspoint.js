bee028f8007b38b98cc9a45082c55edf37cb55a0
Jesse de Vries <jessedvrs@gmail.com>
2015-05-25 17:52:50 +0200

    Renamed _self to Api

--- src/focuspoint.js
+++ src/focuspoint.js
@@ -29,18 +29,18 @@
 
     // View constructor
     function View (viewElm, x, y, options) {
-        var _self = this;
+        var Api = this;
 
         /*****************************************************/
         /* States */
         /*****************************************************/
-        _self.x = .5;
-        _self.y = .5;
+        Api.x = .5;
+        Api.y = .5;
 
         /*****************************************************/
         /* Set state */
         /*****************************************************/
-        _self.set = function set (x, y) {
+        Api.set = function set (x, y) {
             try {
                 if (typeof x !== 'number') throw 'the given x coordinate is not a number';
                 if (typeof y !== 'number') throw 'the given y coordinate is not a number';
@@ -50,14 +50,14 @@
                 var bounded_x = Math.max(0, Math.min(x, 1));
                 var bounded_y = Math.max(0, Math.min(y, 1));
 
-                _self.x = bounded_x;
-                _self.y = bounded_y;
+                Api.x = bounded_x;
+                Api.y = bounded_y;
 
                 _setBackgroundPosition(x, y);
 
                 return true;
             } catch(e) {
-                return helpers.error(_self.options, e);
+                return helpers.error(Api.options, e);
             }
         };
 
@@ -77,32 +77,32 @@
             if (options && ( typeof options !== 'object' || Array.isArray(options) )) throw 'when passing options, it must be an object';
 
             // Options
-            _self.options = {};
-            _self.options.show_errors = options && typeof options.show_errors !== 'undefined' ? options.show_errors : true;
+            Api.options = {};
+            Api.options.show_errors = options && typeof options.show_errors !== 'undefined' ? options.show_errors : true;
 
             // Element validation
             if (!helpers.isElement(viewElm)) throw 'the given view element is not a DOM element';
 
             // Default state
             if(x && y || typeof x === 'number' && typeof y === 'number') {
-                _self.set(x, y);
+                Api.set(x, y);
             }
 
         } catch(e) {
-            return helpers.error(_self.options, e);
+            return helpers.error(Api.options, e);
         }
 
     }
     
     // Edit constructor
     function Edit (containerElm, btnElm, options) {
-        var _self = this;
+        var Api = this;
 
         /*****************************************************/
         /* States */
         /*****************************************************/
-        _self.x = .5;
-        _self.y = .5;
+        Api.x = .5;
+        Api.y = .5;
 
         /*****************************************************/
         /* Listener system */
@@ -126,7 +126,7 @@
             callbacks: []
         };
 
-        _self.on = function on (eventName, cb) {
+        Api.on = function on (eventName, cb) {
             try {
 
                 if (typeof eventName !== 'string') throw 'eventName is not a string';
@@ -147,11 +147,11 @@
                 return id;
 
             } catch(e) {
-                return helpers.error(_self.options, e);
+                return helpers.error(Api.options, e);
             }
         };
 
-        _self.off = function off (id) {
+        Api.off = function off (id) {
             try {
                 var match = null;
                 _listeners.callbacks.forEach(function (o, index) {
@@ -165,14 +165,14 @@
                 return !! _listeners.callbacks.splice(match, 1);
 
             } catch(e) {
-                return helpers.error(_self.options, e);
+                return helpers.error(Api.options, e);
             }
         };
 
         /*****************************************************/
         /* Set state */
         /*****************************************************/
-        _self.set = function set (x, y) {
+        Api.set = function set (x, y) {
             try {
                 if (typeof x !== 'number') throw 'the given x coordinate is not a number';
                 if (typeof y !== 'number') throw 'the given y coordinate is not a number';
@@ -182,14 +182,14 @@
                 var bounded_x = Math.max(0, Math.min(x, 1));
                 var bounded_y = Math.max(0, Math.min(y, 1));
 
-                _self.x = bounded_x;
-                _self.y = bounded_y;
+                Api.x = bounded_x;
+                Api.y = bounded_y;
 
                 _setButtonPosition(bounded_x, bounded_y);
 
                 return true;
             } catch(e) {
-                return helpers.error(_self.options, e);
+                return helpers.error(Api.options, e);
             }
         };
 
@@ -225,7 +225,7 @@
         /*****************************************************/
         /* Attach live background position to image */
         /*****************************************************/
-        _self.attachBackgroundPosition = function attachBackgroundPosition (elm) {
+        Api.attachBackgroundPosition = function attachBackgroundPosition (elm) {
             try {
                 if (!helpers.isElement(elm)) throw 'the given element is not a DOM element';
 
@@ -235,17 +235,17 @@
                     elm.style.backgroundPosition = 'top ' + percent_y + ' left ' + percent_x;
                 };
 
-                var onMoveId = _self.on('move', setToElm);
-                setToElm(_self.x, _self.y);
+                var onMoveId = Api.on('move', setToElm);
+                setToElm(Api.x, Api.y);
 
                 return onMoveId;
 
             } catch(e) {
-                return helpers.error(_self.options, e);
+                return helpers.error(Api.options, e);
             }
         };
 
-        _self.detachBackgroundPosition = _self.off;
+        Api.detachBackgroundPosition = Api.off;
 
         /*****************************************************/
         /* Validate arguments and initialise */
@@ -254,37 +254,37 @@
             if (options && ( typeof options !== 'object' || Array.isArray(options) )) throw 'when passing options, it must be an object';
 
             // Options
-            _self.options = {};
-            _self.options.show_errors = options && typeof options.show_errors !== 'undefined' ? options.show_errors : true;
-            _self.options.hide_cursor = options && typeof options.hide_cursor !== 'undefined' ? options.hide_cursor : true;
-            _self.options.default_x = options && typeof options.default_x !== 'undefined' ? options.default_x : .50;
-            _self.options.default_y = options && typeof options.default_y !== 'undefined' ? options.default_y : .50;
-            _self.options.classnames = {};
-            _self.options.classnames.container_hidecursor = options && options.classnames && typeof options.classnames.container_hidecursor !== 'undefined' ? options.classnames.container_hidecursor : 'lfy-focuspoint-edit-hidecursor';
+            Api.options = {};
+            Api.options.show_errors = options && typeof options.show_errors !== 'undefined' ? options.show_errors : true;
+            Api.options.hide_cursor = options && typeof options.hide_cursor !== 'undefined' ? options.hide_cursor : true;
+            Api.options.default_x = options && typeof options.default_x !== 'undefined' ? options.default_x : .50;
+            Api.options.default_y = options && typeof options.default_y !== 'undefined' ? options.default_y : .50;
+            Api.options.classnames = {};
+            Api.options.classnames.container_hidecursor = options && options.classnames && typeof options.classnames.container_hidecursor !== 'undefined' ? options.classnames.container_hidecursor : 'lfy-focuspoint-edit-hidecursor';
 
             // Element validation
             if (!helpers.isElement(containerElm)) throw 'the given container element is not a DOM element';
             if (!helpers.isElement(btnElm)) throw 'the given button element is not a DOM element';
 
             // Default state
-            _self.set(_self.options.default_x, _self.options.default_y);
+            Api.set(Api.options.default_x, Api.options.default_y);
 
             // Add first mousedown event listener
             btnElm.addEventListener('mousedown', function (event) {
                 var start_pos = _getPositionByEvent(event);
-                _self.set(start_pos.x, start_pos.y);
+                Api.set(start_pos.x, start_pos.y);
                 _listeners.applyFor('start', [
                     start_pos.x,
                     start_pos.y
                 ]);
-                if (_self.options.hide_cursor) {
-                    containerElm.classList.add(_self.options.classnames.container_hidecursor);
+                if (Api.options.hide_cursor) {
+                    containerElm.classList.add(Api.options.classnames.container_hidecursor);
                 }
 
                 var move = function (event) {
                     window.requestAnimationFrame(function () {
                         var move_pos = _getPositionByEvent(event);
-                        _self.set(move_pos.x, move_pos.y);
+                        Api.set(move_pos.x, move_pos.y);
                         _listeners.applyFor('move', [
                             move_pos.x,
                             move_pos.y
@@ -298,7 +298,7 @@
                         stop_pos.x,
                         stop_pos.y
                     ]);
-                    containerElm.classList.remove(_self.options.classnames.container_hidecursor);
+                    containerElm.classList.remove(Api.options.classnames.container_hidecursor);
 
                     document.removeEventListener('mousemove', move);
                     document.removeEventListener('mouseup', up);
@@ -309,7 +309,7 @@
             });
 
         } catch(e) {
-            return helpers.error(_self.options, e);
+            return helpers.error(Api.options, e);
         }
 
     }
