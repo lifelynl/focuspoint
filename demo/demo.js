59e9a9bae8269b198a48ca20752004f1dcb1ac74
Jesse de Vries <jessedvrs@gmail.com>
2015-05-26 11:57:36 +0200

    Removed event selector

--- demo/demo.js
+++ demo/demo.js
@@ -36,17 +36,3 @@ var initViewFocuspoint = function (edit_focuspoint) {
 };
 
 var view_focuspoint = initViewFocuspoint(edit_focuspoint);
-
-// var fpViewElm2 = document.querySelector('.lfy-focuspoint-view.second');
-// var fpViewElm2Select = document.querySelector('[name="view_binder_event_selector"]');
-
-// var fp2 = new Focuspoint.View(fpViewElm2, fp.x, fp.y, {
-//     // options here
-// });
-// var eventId = fp.on(fpViewElm2Select.value, fp2.set);
-
-// // Change the event type (dropdown selector)
-// fpViewElm2Select.addEventListener('change', function () {
-//     fp.off(eventId); // unbind old
-//     eventId = fp.on(this.value, fp2.set); // bind new
-// });
